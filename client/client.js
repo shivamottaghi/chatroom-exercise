a98363a9fc3d34d6f28231bc649eb921ad8561f1
shivamtg <shivamtg1376@gmail.com>
2022-07-19 15:57:37 +0200

    add event listener to send to all button, client.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,14 @@
 let socket = io.connect();
+let inputElement = document.getElementById('message');
+document.getElementById('sendToAll').addEventListener('click',  ()=>{
+    let message = inputElement.value;
+    console.log(message);
+    socket.emit('sendToAll', message);
+    inputElement.value = '';
+});
+
+const target = document.getElementById('messageTarget');
+
+socket.on('displayMessage', (message) => {
+    target.innerHTML += '<br>'+message;
+});
