4eab6fa5c08a8c40cfe7aab928d74f513bcbe756
shivamtg <shivamtg1376@gmail.com>
2022-07-19 16:12:19 +0200

    add event listener to send private button in client.js

--- client/client.js
+++ client/client.js
@@ -1,12 +1,20 @@
 let socket = io.connect();
+
+
 let inputElement = document.getElementById('message');
+
+//event listener for send to all
 document.getElementById('sendToAll').addEventListener('click',  ()=>{
     let message = inputElement.value;
     console.log(message);
     socket.emit('sendToAll', message);
     inputElement.value = '';
 });
-
+document.getElementById('sendPrivate').addEventListener('click' , ()=>{
+    let message = inputElement.value;
+    socket.emit('sendToMe', message);
+    inputElement.value='';
+});
 const target = document.getElementById('messageTarget');
 
 socket.on('displayMessage', (message) => {
