7ad9f3bad3d2cf49bb038e7a87640a3f3d4aa7ef
shivamtg <shivamtg1376@gmail.com>
2022-07-20 09:50:23 +0200

    add fuctionality to send user name to server, client.js

--- client/client.js
+++ client/client.js
@@ -1,22 +1,22 @@
 let socket = io.connect();
 
-
+let userName = prompt('What is your user name?');
 let inputElement = document.getElementById('message');
 
 //event listener for send to all
 document.getElementById('sendToAll').addEventListener('click',  ()=>{
     let message = inputElement.value;
-    console.log(message);
-    socket.emit('sendToAll', message);
+    //console.log(message);
+    socket.emit('sendToAll', (userName + " : " + message));
     inputElement.value = '';
 });
 document.getElementById('sendPrivate').addEventListener('click' , ()=>{
     let message = inputElement.value;
-    socket.emit('sendToMe', message);
+    socket.emit('sendToMe', (userName + " : " + message));
     inputElement.value='';
 });
 const target = document.getElementById('messageTarget');
 
 socket.on('displayMessage', (message) => {
-    target.innerHTML += '<br>'+message;
+    target.innerHTML += '<br>' + message;
 });
