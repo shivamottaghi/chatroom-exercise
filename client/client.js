827d37b0fb5e81683688a9cae0461cbdd72b229d
shivamtg <shivamtg1376@gmail.com>
2022-07-20 15:02:19 +0200

    display username input on load, client.js

--- client/client.js
+++ client/client.js
@@ -1,43 +1,19 @@
-let socket = io.connect();
-let userName = prompt('What is your user name?');
-let isUsernameSet = false;
-/*let userName = '';
+//*****************************my functions
+const displayLogin = () => {
+    let loginRow = document.getElementById('loginRow');
+    loginRow.classList.remove('d-none');
+}
 
-
-if (isUsernameSet = false) {
-    displayLoginPage();
-    //isUsernameSet = true;
-}*/
-socket.emit('setUserName', userName);
-let inputElement = document.getElementById('message');
-
-//event listener for send to all
-document.getElementById('sendToAll').addEventListener('click', () => {
-    let message = inputElement.value;
-    //console.log(message);
-    socket.emit('sendToAll', (message));
-    inputElement.value = '';
-});
-document.getElementById('sendPrivate').addEventListener('click', () => {
-    let message = inputElement.value;
-    socket.emit('sendToMe', (message));
-    inputElement.value = '';
-});
-const target = document.getElementById('messageTarget');
-
-//socket functions
-socket.on('displayMessage', (message) => {
-    target.innerHTML += '<br>' + message;
-});
-socket.on('displayUsers', usersArr => {
-    displayUsers(usersArr);
-    console.log(usersArr)
-});
-
-//my functions
-const displayLoginPage = () => {
+const displayChatBox = () => {
+    let chatBoxRow = document.getElementById('chatBoxRow')
+    chatBoxRow.classList.remove('d-none');
+    let loginRow = document.getElementById('loginRow');
+    loginRow.classList.add('d-none');
+    let displayUsers = document.getElementById('displayUsers');
+    displayUsers.classList.remove('d-none')
 
 }
+
 const displayUsers = (arr) => {
     console.log(arr)
     let userElement = document.getElementById('displayUsers');
@@ -52,3 +28,45 @@ const displayUsers = (arr) => {
 
     });
 }
+
+
+
+let socket = io.connect();
+//let userName = prompt('What is your user name?');
+let isUsernameSet = false;
+let userName = '';
+displayLogin();
+
+/*if (isUsernameSet = false) {*/
+/*    //isUsernameSet = true;
+}*/
+socket.emit('setUserName', userName);
+let inputElement = document.getElementById('message');
+
+//*****************************event listeners
+document.getElementById('sendToAll').addEventListener('click', () => {
+    let message = inputElement.value;
+    //console.log(message);
+    socket.emit('sendToAll', (message));
+    inputElement.value = '';
+});
+document.getElementById('sendPrivate').addEventListener('click', () => {
+    let message = inputElement.value;
+    socket.emit('sendToMe', (message));
+    inputElement.value = '';
+});
+document.getElementById('login').addEventListener('click', ()=>{
+     userName = document.getElementById('username').value;
+     isUsernameSet = true;
+     displayChatBox();
+});
+//*****************************socket functions
+const target = document.getElementById('messageTarget');
+socket.on('displayMessage', (message) => {
+    target.innerHTML += '<br>' + message;
+});
+socket.on('displayUsers', usersArr => {
+    displayUsers(usersArr);
+    console.log(usersArr)
+});
+
